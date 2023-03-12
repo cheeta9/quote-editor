d5a3aebd9c289ea90d863f163822ba8fa763ce76
TB-takahiro-sakaguchi <takahiro_sakaguchi@tbn.co.jp>
2023-03-12 13:37:49 +0900

    feat:turbo streams and security

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,11 +1,13 @@
 class Quote < ApplicationRecord
+  belongs_to :company
+
   # after_create_commit -> { broadcast_prepend_later_to 'quotes', partial: 'quotes/quote', locals: { quote: self }, target: 'quotes' }
   # More shorter...
   # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
   # after_update_commit -> { broadcast_replace_later_to 'quotes' }
   # after_destroy_commit -> { broadcast_remove_to 'quotes' }
   # Those three callbacks are equivalent to the following single line
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
   validates :name, presence: true
 
